8bbd0a5daabb1405dd69db975b8cd4d0804c21a4
Salpodnag <mla40@yandex.ru>
2023-05-29 16:31:16 +0300

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string cont;
+
+
+                double fn;
+                double sn;
+                string o;
+                double answ;
+
+                Console.Clear();
+                Console.WriteLine("Привет!");
+                Console.WriteLine("Первое число: ");
+                fn = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Действие %, *, /, +, - : ");
+                o = Convert.ToString(Console.ReadLine());
+
+                Console.WriteLine("Второе число: ");
+                sn = Convert.ToDouble(Console.ReadLine());
+
+                if (o == "+")
+                {
+
+                    answ = fn + sn;
+                    Console.WriteLine("Результат: " + answ);
+
+                }
+
+                if (o == "-")
+                {
+
+                    answ = fn - sn;
+                    Console.WriteLine("Результат: " + answ);
+
+                }
+
+                if (o == "*")
+                {
+
+                    answ = fn * sn;
+                    Console.WriteLine("Результат: " + answ);
+
+                }
+
+                if (o == "/")
+                {
+
+                    answ = fn / sn;
+                    Console.WriteLine("Результат: " + answ);
+
+                }
+
+                if (o == "%")
+                {
+
+                    answ = fn % sn;
+                    Console.WriteLine("Результат: " + answ);
+
+                }
+
+                // Console.WriteLine("Совершить еще одну операцию?");
+              //  cont = Convert.ToString(Console.ReadLine());
+
+        }
+    }
+}
